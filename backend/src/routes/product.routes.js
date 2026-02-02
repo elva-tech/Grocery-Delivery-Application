68f46ac69a1456d225139c56e7897973bb9eb83d
chethankumar <chethankumarhr.1si22is020@gmail.com>
2026-02-02 18:43:20 +0530

    wip: local work before pull

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { addProduct } = require("../controllers/product.controller");
 const { authMiddleware, adminOnly } = require("../middleware/auth.middleware");
-
+const { updateProduct } = require("../controllers/update.controller");
 router.post("/admin/products", authMiddleware, adminOnly, addProduct);
-
+router.put("/admin/products/:id", authMiddleware, adminOnly, updateProduct);
 module.exports = router;
