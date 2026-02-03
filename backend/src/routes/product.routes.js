c9feaa7053464863a91a2e6e69d156492c9dd209
chethankumar <chethankumarhr.1si22is020@gmail.com>
2026-02-03 14:04:10 +0530

    Resolved all comments raised and added the updateProductFromAdmin to product.controller

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-const { addProduct } = require("../controllers/product.controller");
+const {
+  addProduct,
+  updateProductFromAdmin
+} = require("../controllers/product.controller");
+
 const { authMiddleware, adminOnly } = require("../middleware/auth.middleware");
-const { updateProduct } = require("../controllers/update.controller");
-router.post("/admin/products", authMiddleware, adminOnly, addProduct);
-router.put("/admin/products/:id", authMiddleware, adminOnly, updateProduct);
+
+router.post(
+  "/admin/products",
+  authMiddleware,
+  adminOnly,
+  addProduct
+);
+
+router.put(
+  "/admin/products/:id",
+  authMiddleware,
+  adminOnly,
+  updateProductFromAdmin
+);
 module.exports = router;
